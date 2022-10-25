2d83ebd3fba29e1db95f279fed94474c1ddd8b6c
shadow-code <ns13030130@gmail.com>
2022-10-25 10:24:51 +0000

    added some npm packages

--- src/pages/Registration/LicenseInfo/main.tsx
+++ src/pages/Registration/LicenseInfo/main.tsx
@@ -8,43 +8,61 @@ type InputTypeProps = {
 }
 
 const LicenseInfoPage = () => {
-    const {formData, setFormData} = React.useContext(FormContext)
+    const { formData, setFormData } = React.useContext(FormContext)
 
-    function onInputChange({e, type}: InputTypeProps){
-        const data = {...formData, [type]:e.target.value} 
+    function onInputChange({ e, type }: InputTypeProps) {
+        const data = { ...formData, [type]: e.target.value }
         setFormData(data)
     }
     return (
         <>
             <form action="">
                 <div className="grid grid-cols-2 mb-3">
-                    <div className="flex flex-col w-[88%]">
-                        <label htmlFor="file" className="text-gray-600 text-sm mb-4">* First Name</label>
-                        <input type="file" id="file" onChange={(e)=> onInputChange({e, type: "dl_front"})} className="border-none rounded-lg h-10 px-4 text-sm outline-none" placeholder="Kenneth" />
+                    <div className="flex flex-col w-[88%] pb-3">
+                    <label htmlFor="" className="text-gray-600 text-sm mb-4">* Driver's license front</label>
+                        <div className="flex justify-center items-center w-full">
+                            <label htmlFor="dropzone-file" className="flex flex-col justify-center items-center w-full h-32 bg-gray-50 rounded-lg border-2 border-gray-300 border-dashed cursor-pointer dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+                                <div className="flex flex-col justify-center items-center pt-5 pb-6">
+                                    <svg aria-hidden="true" className="mb-3 w-10 h-10 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"></path></svg>
+                                    <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload</span> or drag and drop</p>
+                                    <p className="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
+                                </div>
+                                <input id="dropzone-file" type="file" className="" />
+                            </label>
+                        </div>
                     </div>
                     <div className="flex flex-col w-[88%]">
-                        <label htmlFor="" className="text-gray-600 text-sm mb-4">* Last Name</label>
-                        <input type="file" onChange={(e)=> onInputChange({e, type: "dl_front"})} className="rounded-lg h-10 px-4 text-sm outline-none" placeholder="Lartey" />
+                    <label htmlFor="" className="text-gray-600 text-sm mb-4">* Driver's license back</label>
+                        <div className="flex justify-center items-center w-full">
+                            <label htmlFor="dropzone-file" className="flex flex-col justify-center items-center w-full h-32 bg-gray-50 rounded-lg border-2 border-gray-300 border-dashed cursor-pointer dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+                                <div className="flex flex-col justify-center items-center pt-5 pb-6">
+                                    <svg aria-hidden="true" className="mb-3 w-10 h-10 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"></path></svg>
+                                    <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload</span> or drag and drop</p>
+                                    <p className="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
+                                </div>
+                                <input id="dropzone-file" type="file" className="" />
+                            </label>
+                        </div>
                     </div>
                 </div>
                 <div className="grid grid-cols-2 mb-3">
                     <div className="flex flex-col w-[88%]">
                         <label htmlFor="" className="text-gray-600 text-sm mb-4">* Driver's license number</label>
-                        <input type="text" onChange={(e)=> onInputChange({e, type: "license"})}  className="rounded-lg h-10 px-4 text-sm outline-none" placeholder="Haatso" />
+                        <input type="text" onChange={(e) => onInputChange({ e, type: "license" })} className="rounded-lg h-10 px-4 text-sm outline-none" placeholder="Haatso" />
                     </div>
                     <div className="flex flex-col w-[88%]">
                         <label htmlFor="" className="text-gray-600 text-sm mb-4">* Driver's license expiration date</label>
-                        <input type="text" onChange={(e)=> onInputChange({e, type: "expirationDate"})} className="rounded-lg h-10 px-4 text-sm outline-none" placeholder="+233268211334" />
+                        <input type="text" onChange={(e) => onInputChange({ e, type: "expirationDate" })} className="rounded-lg h-10 px-4 text-sm outline-none" placeholder="+233268211334" />
                     </div>
                 </div>
                 <div className="grid grid-cols-2 mb-3">
                     <div className="flex flex-col w-[88%]">
                         <label htmlFor="" className="text-gray-600 text-sm mb-4">* Vehicle type</label>
-                        <input type="text" onChange={(e)=> onInputChange({e, type: "vehicleType"})} className="rounded-lg h-10 text-sm px-4 outline-none" placeholder="Stacy" />
+                        <input type="text" onChange={(e) => onInputChange({ e, type: "vehicleType" })} className="rounded-lg h-10 text-sm px-4 outline-none" placeholder="Stacy" />
                     </div>
                     <div className="flex flex-col w-[88%]">
                         <label htmlFor="" className="text-gray-600 text-sm mb-4">* National ID(Ghana Card)</label>
-                        <input type="text" onChange={(e)=> onInputChange({e, type: "nationalID"})} className="rounded-lg h-10 text-sm px-4 outline-none" placeholder="+233268211333" />
+                        <input type="text" onChange={(e) => onInputChange({ e, type: "nationalID" })} className="rounded-lg h-10 text-sm px-4 outline-none" placeholder="+233268211333" />
                     </div>
                 </div>
                 <div className="flex justify-between w-[94%] mt-6">
--- src/pages/Registration/PersonalInfo/main.tsx
+++ src/pages/Registration/PersonalInfo/main.tsx
@@ -13,22 +13,22 @@ const PersonalInfoPage = ({ setFristName, setLastName, setEmail }: Props) => {
 
     const { formData, setFormData } = useContext(FormContext)
 
-    function firstName(e: React.ChangeEvent<HTMLInputElement>){
+    function firstName(e: React.ChangeEvent<HTMLInputElement>) {
         const firstName = e.target.value
         setFristName(firstName)
-        setFormData({...formData, firstName: firstName})
+        setFormData({ ...formData, firstName: firstName })
     }
 
-    function lastName(e: React.ChangeEvent<HTMLInputElement>){
+    function lastName(e: React.ChangeEvent<HTMLInputElement>) {
         const lastName = e.target.value
         setLastName(lastName)
-        setFormData({...formData, lastName: lastName})
+        setFormData({ ...formData, lastName: lastName })
     }
 
-    function email(e: React.ChangeEvent<HTMLInputElement>){
+    function email(e: React.ChangeEvent<HTMLInputElement>) {
         const email = e.target.value
         setEmail(email)
-        setFormData({...formData, email: email})
+        setFormData({ ...formData, email: email })
     }
 
     return (
@@ -51,30 +51,35 @@ const PersonalInfoPage = ({ setFristName, setLastName, setEmail }: Props) => {
                     </div>
                     <div className="flex flex-col w-[88%]">
                         <label htmlFor="" className="text-gray-600 text-sm mb-4">* Mobile Phone</label>
-                        <input type="text" onChange={(e) => { setFormData({ ...formData, msisdn: e.target.value})}} className="rounded-lg h-10 px-4 text-sm outline-none" placeholder="+233268211334" />
+                        <input type="text" onChange={(e) => { setFormData({ ...formData, msisdn: e.target.value }) }} className="rounded-lg h-10 px-4 text-sm outline-none" placeholder="+233268211334" />
                     </div>
                 </div>
                 <div className="grid grid-cols-2 mb-3">
                     <div className="flex flex-col w-[88%]">
                         <label htmlFor="" className="text-gray-600 text-sm mb-4">* Actual Address</label>
-                        <input type="text" onChange={(e) => { setFormData({ ...formData, address: e.target.value})}} className="rounded-lg h-10 px-4 text-sm outline-none" placeholder="Haatso" />
+                        <input type="text" onChange={(e) => { setFormData({ ...formData, address: e.target.value }) }} className="rounded-lg h-10 px-4 text-sm outline-none" placeholder="Haatso" />
                     </div>
                     <div className="flex w-[88%]">
                         <div className="w-[80%]">
                             <label htmlFor="" className="text-gray-600 text-sm mb-4">* Date of birth</label>
-                            {/* <DatePicker selected={date} onChange={(date):ReactDatePickerProps<never, undefined>=> setDate(date)} className="rounded-lg"/> */}
-                            <input type="text" className="rounded-lg h-10 w-[100%]" />
+                            <div className="relative">
+                                <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+                                    <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
+                                </div>
+                                <div inline-datepicker data-date="02/25/2022"></div>
+                                <input datepicker-buttons type="text" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Select date" />
+                            </div>
                         </div>
                         <div className="mx-2 w-[50%]">
                             <label htmlFor="" className="text-gray-600 text-sm mb-4">* Gender</label>
-                            <select onChange={(e)=>{ setFormData({ ...formData, gender: e.target.value})}} className="rounded-lg h-10 w-[100%] bg-white outline-none">
+                            <select onChange={(e) => { setFormData({ ...formData, gender: e.target.value }) }} className="rounded-lg h-10 w-[100%] bg-white outline-none">
                                 <option value="male">male</option>
                                 <option value="femail">female</option>
                             </select>
                         </div>
                         <div className="w-[60%]">
                             <label htmlFor="" className="text-gray-600 text-sm mb-4">* Marital Status</label>
-                            <select onChange={(e)=>{ setFormData({ ...formData, maritalStatus: e.target.value})}} className="rounded-lg h-10 w-[100%] bg-white outline-none">
+                            <select onChange={(e) => { setFormData({ ...formData, maritalStatus: e.target.value }) }} className="rounded-lg h-10 w-[100%] bg-white outline-none">
                                 <option value="single">single</option>
                                 <option value="married">married</option>
                                 <option value="s">searching</option>
@@ -85,11 +90,11 @@ const PersonalInfoPage = ({ setFristName, setLastName, setEmail }: Props) => {
                 <div className="grid grid-cols-2 mb-3">
                     <div className="flex flex-col w-[88%]">
                         <label htmlFor="" className="text-gray-600 text-sm mb-4">* Emmergency contact name</label>
-                        <input type="text" onChange={(e) => { setFormData({ ...formData, emergencyContactName: e.target.value})}} className="rounded-lg h-10 text-sm px-4 outline-none" placeholder="Stacy" />
+                        <input type="text" onChange={(e) => { setFormData({ ...formData, emergencyContactName: e.target.value }) }} className="rounded-lg h-10 text-sm px-4 outline-none" placeholder="Stacy" />
                     </div>
                     <div className="flex flex-col w-[88%]">
                         <label htmlFor="" className="text-gray-600 text-sm mb-4">* Emergency contact number</label>
-                        <input type="text" onChange={(e) => { setFormData({ ...formData, emergencyContactNumber: e.target.value})}} className="rounded-lg h-10 text-sm px-4 outline-none" placeholder="+233268211333" />
+                        <input type="text" onChange={(e) => { setFormData({ ...formData, emergencyContactNumber: e.target.value }) }} className="rounded-lg h-10 text-sm px-4 outline-none" placeholder="+233268211333" />
                     </div>
                 </div>
                 <div className="flex justify-end w-[94%] mt-5">
